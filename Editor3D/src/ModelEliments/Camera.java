7136d6f349684ab7247ae1aa5bce08eb22ccdc33
Poskale <100698351+Poskale@users.noreply.github.com>
2023-11-02 23:29:32 +0500

    Update Camera.java

--- Editor3D/src/ModelEliments/Camera.java
+++ Editor3D/src/ModelEliments/Camera.java
@@ -1,5 +1,14 @@
 package ModelEliments;
 
 public class Camera {
-    
+      private Point3D location;
+    private Angle3D angle;
+
+    public void rotate(Angle3D angle) {
+        // Implementation of the rotate method
+    }
+
+    public void move(Point3D point) {
+        // Implementation of the move method
+    }
 }
