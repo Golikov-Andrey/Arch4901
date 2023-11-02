6ece3c0ea232a1f4f7879fe664f821eaa60d4f95
Poskale <100698351+Poskale@users.noreply.github.com>
2023-11-02 23:29:20 +0500

    Update Flash.java

--- Editor3D/src/ModelEliments/Flash.java
+++ Editor3D/src/ModelEliments/Flash.java
@@ -1,5 +1,16 @@
 package ModelEliments;
 
 public class Flash {
-    
+    private Point3D location;
+    private Angle3D angle;
+    private Color color;
+    private float power;
+
+    public void rotate(Angle3D angle) {
+        // Implementation of the rotate method
+    }
+
+    public void move(Point3D point) {
+        // Implementation of the move method
+    }
 }
