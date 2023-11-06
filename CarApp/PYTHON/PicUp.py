bb6ab4232241f82aa1d2350103ccdac5bc29cf71
Golikov-Andrey <newsc2@yandex.ru>
2023-11-07 00:29:09 +0300

    solution from seminar 3 architect software

---
+++ CarApp/PYTHON/PicUp.py
@@ -0,0 +1,19 @@
+from Car import Car
+from ICarWash import ICarWash
+from IFuelStation import IFuelStation
+
+
+class PickUp(Car, IFuelStation, ICarWash):
+    load_capacity: int
+
+    def __init__(self, brand, model, color, body_type, wheel_count, fuel_type, transmission_type, engine_capacity,
+                 load_capacity):
+        super().__init__(brand, model, color, body_type, wheel_count, fuel_type, transmission_type, engine_capacity)
+        self.load_capacity = load_capacity
+
+    @staticmethod
+    def sweep_street():
+        print('Подмееетаааееем, вжух, вжух)')
+
+    def refill(self):
+        print('Заправляемся, бульк)')
